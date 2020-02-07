8d15bd5201815e9db3d30d0d3ef594a85a9379e2
Przemek Grochowski <sovvly@protonmail.com>
2020-02-07 19:22:39 +0100

    new timer fun

--- app/src/main/java/com/hfad/stopwatch/MainActivity.kt
+++ app/src/main/java/com/hfad/stopwatch/MainActivity.kt
@@ -2,6 +2,7 @@ package com.hfad.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.widget.TextView
 
@@ -9,11 +10,39 @@ class MainActivity : AppCompatActivity() {
 
     private var seconds = 0
     private var running = false
+    private var wasRunning = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds")
+            running = savedInstanceState.getBoolean("running")
+            wasRunning = savedInstanceState.getBoolean("wasRunning")
+        }
+
         runTimer()
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        savedInstanceState.putInt("seconds", seconds)
+        savedInstanceState.putBoolean("runnings", running)
+        savedInstanceState.putBoolean("wasRunning", wasRunning)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (wasRunning) {
+            running = true
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        wasRunning = running
+        running = false
     }
 
     fun onClickStart(view: View) {
@@ -29,19 +58,28 @@ class MainActivity : AppCompatActivity() {
         seconds = 0
     }
 
-    val timeView: TextView = findViewById(R.id.time) as TextView
-
     fun runTimer() {
+        val timeView: TextView = findViewById(R.id.time)
+        val handler = Handler()
 
-        var hours = seconds/3600
-        var minutes = (seconds%3600)/60
-        var seconds = seconds%60
 
-        var time = String.format("%d:%02d:%02d", hours, minutes, seconds)
-        timeView.setText(time)
+        handler.post(object: Runnable {
+            override fun run() {
+                val hours = seconds / 3600
+                val minutes = (seconds % 3600) / 60
+                val sec = seconds % 60
 
-            if(running){
-                seconds++
+
+                val time = String.format("%d:%02d:%02d", hours, minutes, sec)
+                timeView.text = time
+
+                if (running) {
+                    seconds = seconds.inc()
+                }
+                println("Sekundy!!!!!   $seconds")
+                handler.postDelayed(this, 1000)
             }
+        })
     }
 }
+
